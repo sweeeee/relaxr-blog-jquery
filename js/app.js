212a605ad2f646509823cb4f7446a8f29be545b3
sweeeee <sweehan@Sweehan-Moks-MacBook-Pro.local>
2017-03-05 18:11:02 +0800

    add javascript

---
+++ js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $('.readmore').on('click', function(event){
+    event.preventDefault();
+    var text = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    $('.post').append(text);
+    $(this).remove();
+  });
+});
